d1fb7921adf6f6cb064b4584b1f42a41c42d0a2f
jewonyeon <yjw9852@gmail.com>
2022-05-22 19:10:12 +0900

    Feat Keyboard Event

--- Part1/1.virtual-keyboard/src/js/Keyboard.js
+++ Part1/1.virtual-keyboard/src/js/Keyboard.js
@@ -2,21 +2,30 @@ export default class Keyboard {
   #containerEl;
   #switchEl;
   #fontSelectEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
 
   constructor() {
-    this.#assingElement();
+    this.#assignElement();
     this.#addEvent();
   }
 
-  #assingElement() {
+  #assignElement() {
     this.#containerEl = document.getElementById('container');
     this.#switchEl = this.#containerEl.querySelector('#switch');
     this.#fontSelectEl = this.#containerEl.querySelector('#font');
+    this.#keyboardEl = this.#containerEl.querySelector('#keyboard');
+    this.#inputGroupEl = this.#containerEl.querySelector('#input-group');
+    this.#inputEl = this.#inputGroupEl.querySelector('#input');
   }
 
   #addEvent() {
     this.#switchEl.addEventListener('change', this.#onChangeTheme);
     this.#fontSelectEl.addEventListener('change', this.#onChangeFont);
+    document.addEventListener('keydown', this.#onKeyDown.bind(this));
+    document.addEventListener('keyup', this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener('input', this.#onInput);
   }
 
   #onChangeTheme(event) {
@@ -28,5 +37,26 @@ export default class Keyboard {
 
   #onChangeFont(event) {
     document.body.style.fontFamily = event.target.value;
+  }
+
+  #onKeyDown(event) {
+    this.#inputGroupEl.classList.toggle(
+      'error',
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key),
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add('active');
+  }
+
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove('active');
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, '');
   }
 }
